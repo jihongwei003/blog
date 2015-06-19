bd134313876af850c0479beb38d0a23664e2cf81
jihongwei003 <837295194@qq.com>
2015-06-19 21:18:06 +0800

    again

--- models/Persist.js
+++ models/Persist.js
@@ -2,16 +2,33 @@ function Conn() {
 }
 module.exports = Conn;
 
-Conn.db_name = "blog";
+var db_name = "blog";
 
-Conn.connection_string = 'mongodb://127.0.0.1:27017/' + Conn.db_name;
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    this.connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME +
-        ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD +
-        "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":27107" + Conn.db_name;
+var user = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+var password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+var host = process.env.OPENSHIFT_MONGODB_DB_HOST;
+
+if (typeof user === "undefined") {
+    console.warn('No OPENSHIFT_MONGODB_DB_USERNAME var, using ""');
+    user = "";
+}
+if (typeof password === "undefined") {
+    console.warn('No OPENSHIFT_MONGODB_DB_PASSWORD var, using ""');
+    password = "";
+}
+if (typeof host === "undefined") {
+    console.warn('No OPENSHIFT_MONGODB_DB_HOST var, using "127.0.0.1"');
+    host = "127.0.0.1";
 }
 
+Conn.connection_string = 'mongodb://' + user + ':' + password + '@' + host + ':27017/' + db_name;
+
+//Conn.connection_string =
+//    'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME +
+//    ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD +
+//    "@" + process.env.OPENSHIFT_MONGODB_DB_HOST +
+//    ":27107" + Conn.db_name;
+
 Conn.get = function () {
-    console.log("Mongodb URL:" + this.connection_string);
     return this.connection_string;
 }
