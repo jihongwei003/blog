29db075d66aafaba0b787a7a6a1144ae2fa73cf0
jihongwei003 <837295194@qq.com>
2015-06-19 16:26:51 +0800

    modify persist.js

--- models/Persist.js
+++ models/Persist.js
@@ -2,7 +2,7 @@ function Conn(){
 }
 module.exports = Conn;
 
-Conn.db_name = "mydb";
+Conn.db_name = "blog";
 Conn.mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + Conn.db_name;
 if (process.env.OPENSHIFT_MONGODB_DB_URL) {
     this.mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + Conn.db_name;
