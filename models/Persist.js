af1716da924ce155f5011f897c12246849a88a5c
jihongwei003 <837295194@qq.com>
2015-06-19 17:16:12 +0800

    again

--- models/Persist.js
+++ models/Persist.js
@@ -3,7 +3,7 @@ function Conn(){
 module.exports = Conn;
 
 Conn.db_name = "blog";
-Conn.mongodb_connection_string = 'mongodb://127.4.95.2/' + Conn.db_name;
+Conn.mongodb_connection_string = 'mongodb://127.4.95.2:27017/' + Conn.db_name;
 if (process.env.OPENSHIFT_MONGODB_DB_URL) {
     this.mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + Conn.db_name;
 }
