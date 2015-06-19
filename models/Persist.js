ebd0af64d72c691dca5972fa15cd0429b14b271a
jihongwei003 <837295194@qq.com>
2015-06-19 20:58:31 +0800

    again

--- models/Persist.js
+++ models/Persist.js
@@ -4,16 +4,14 @@ module.exports = Conn;
 
 Conn.db_name = "blog";
 
-//Conn.mongodb_connection_string = 'mongodb://127.4.95.2:27017/' + Conn.db_name;
-//if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-//    this.mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + Conn.db_name;
-//}
-
-Conn.connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME +
-    ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD +
-    "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":27107" + Conn.db_name
-    || 'mongodb://127.0.0.1:27017/' + Conn.db_name;
+Conn.connection_string = 'mongodb://127.0.0.1:27017/' + Conn.db_name;
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    this.connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME +
+        ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD +
+        "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":27107" + Conn.db_name;
+}
 
 Conn.get = function () {
+    console.log("Mongodb URL:" + this.connection_string);
     return this.connection_string;
 }
